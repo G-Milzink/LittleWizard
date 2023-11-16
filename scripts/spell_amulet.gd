bd67e1499554badb2debd5efaa8ccb175a82d364
Gustaaf Milzink <107410648+G-Milzink@users.noreply.github.com>
2023-11-16 15:42:56 +0100

    Fix spell selection bug: selectyion area remained active while menu was closed. Create platform-spell-scene/artwork

--- scripts/spell_amulet.gd
+++ scripts/spell_amulet.gd
@@ -1,22 +1,38 @@
 extends Node2D
 
 @onready var amulet = $"."
+
 @onready var xtal_orange = $xtal_orange
 @onready var xtal_green = $xtal_green
 @onready var xtal_blue = $xtal_blue
 @onready var xtal_purple = $xtal_purple
 
+@onready var area_orange = $area_orange
+@onready var area_green = $area_green
+@onready var area_blue = $area_blue
+@onready var area_purple = $area_purple
+
+
 var previous_spell
 
 func _ready():
 	amulet.visible = false
-	xtal_orange.visible = false
 
 func _process(delta):
+	print(_Globals.current_spell)
 	_Display_Current_Spell()
 	_Detect_Input()
 	if !amulet.visible:
 		position = get_global_mouse_position()
+		area_orange.set_monitoring(false)
+		area_green.set_monitoring(false)
+		area_blue.set_monitoring(false)
+		area_purple.set_monitoring(false)
+	else:
+		area_orange.set_monitoring(true)
+		area_green.set_monitoring(true)
+		area_blue.set_monitoring(true)
+		area_purple.set_monitoring(true)
 
 func _Detect_Input():
 	if Input.is_action_just_pressed("spell_select"):
