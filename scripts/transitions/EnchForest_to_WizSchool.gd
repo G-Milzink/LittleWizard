7a6f03e1543317e6403bbc6ec70f9c31a208e900
Gustaaf Milzink <107410648+G-Milzink@users.noreply.github.com>
2023-11-18 23:48:51 +0100

    expand enchanted forest tileset, adjust maps, rework door transition system

--- scripts/transitions/EnchForest_to_WizSchool.gd
+++ scripts/transitions/EnchForest_to_WizSchool.gd
@@ -3,8 +3,14 @@ extends Node2D
 @onready var current_map = $"../.."
 @onready var world = $"../../.."
 
+var in_range = false
+
+func _process(delta):
+	if in_range:
+		if Input.is_action_just_pressed("action"):
+			world._Transition_EnchForest2WizSchool()
+			current_map.queue_free()
 
 func _on_area_2d_body_entered(body):
 	if body.name == "Player":
-		world._Transition_EnchForest2WizSchool()
-		current_map.queue_free()
+		in_range = true
--- scripts/transitions/WizSchool_to_EnchForest.gd
+++ scripts/transitions/WizSchool_to_EnchForest.gd
@@ -3,7 +3,15 @@ extends Node2D
 @onready var current_map = $"../.."
 @onready var world = $"../../.."
 
-func _on_area_2d_body_entered(body):
-		if body.name == "Player":
+var in_range = false
+
+func _process(delta):
+	if in_range:
+		if Input.is_action_just_pressed("action"):
 			world._Transition_WizSchool2EnchForest()
 			current_map.queue_free()
+
+func _on_area_2d_body_entered(body):
+	if body.name == "Player":
+		in_range = true
+
