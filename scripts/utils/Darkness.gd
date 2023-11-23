4096dbd6d773d41f4fb60ef758443c4c690b5ea5
Gustaaf Milzink <107410648+G-Milzink@users.noreply.github.com>
2023-11-23 17:54:10 +0100

    Expand darkness system to darken entire screen upon entering area

--- scripts/utils/Darkness.gd
+++ scripts/utils/Darkness.gd
@@ -1,9 +1,27 @@
 extends Node2D
 
+@export var canvas_modulate: Node
+@export var base_color: Color
+@export var dark_color: Color
 
+var current_color
+
+func _ready():
+	current_color = base_color
 
 func _on_area_2d_body_entered(body):
-	_Globals.pixie_in_darkness = true
+	if body.name == "Pixie":
+		_Globals.pixie_in_darkness = true
+	if body.name == "Player":
+		current_color = dark_color
 
 func _on_area_2d_body_exited(body):
-	_Globals.pixie_in_darkness = false
+	if body.name == "Pixie":
+		_Globals.pixie_in_darkness = false
+	if body.name == "Player":
+		current_color = base_color
+
+
+func _process(delta):
+	var tween = get_tree().create_tween()
+	tween.tween_property(canvas_modulate, "color", current_color, .66)
