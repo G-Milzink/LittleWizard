8e41ce1258a0c4de657fa204eab0bd6f26229307
Gustaaf Milzink <107410648+G-Milzink@users.noreply.github.com>
2023-11-28 00:19:17 +0100

    Add animated icon to level transition

--- scripts/transitions/WizSchool_to_EnchForest.gd
+++ scripts/transitions/WizSchool_to_EnchForest.gd
@@ -2,8 +2,12 @@ extends Node2D
 
 @onready var current_map = $"../.."
 @onready var world = $"../../.."
+@onready var arrow = $Arrow
 
 var in_range = false
+
+func _ready():
+	arrow.visible = false
 
 func _process(delta):
 	if in_range:
@@ -15,8 +19,10 @@ func _process(delta):
 func _on_area_2d_body_entered(body):
 	if body.name == "Player":
 		in_range = true
+		arrow.visible = true 
 
 
 func _on_area_2d_body_exited(body):
 	if body.name == "Player":
 		in_range = false
+		arrow.visible = false
