aee9c731c8603dea6c370230ae622c2ed0f66130
Gustaaf Milzink <107410648+G-Milzink@users.noreply.github.com>
2023-11-30 17:41:24 +0100

    fix main scene canvaslayer structure, reinstate splach screen (static)

--- scripts/Menu.gd
+++ scripts/Menu.gd
@@ -1,13 +1,15 @@
 extends Control
 
-@onready var splash = $CanvasLayer/Splash
+@onready var splash = $Splash
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	splash.visible = _Globals.show_splash
+	#splash.visible = _Globals.show_splash
+	pass
 
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	splash.visible = _Globals.show_splash
+	#splash.visible = _Globals.show_splash
+	pass
